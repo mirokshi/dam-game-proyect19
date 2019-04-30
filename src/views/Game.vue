6d6a60d5977a6b3ec7e9dd5b62b0a65930d55251
mirokshi <rojasdiazmirokshi@gmail.com>
2019-04-30 21:30:10 +0200

    Sync

---
+++ src/views/Game.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="game"></div>
+</template>
+
+<script>
+  import Phaser from 'phaser'
+  import background from '../assets/background.jpg'
+  export default {
+    name: 'Game',
+    mounted () {
+      // PHASER 2.6 -> phaser-ce
+      // PHASER 3.0 -> phaser
+      let config = {
+        type: Phaser.AUTO,
+        width: 500,
+        height: 200,
+        physics: {
+          default: 'arcade',
+          arcade: {
+            gravity: { y: 200 }
+          }
+        },
+        // NO HI HA STATES A 3.0 -> SCENES
+        scene: {
+          preload () {
+              this.load.image('background', background)
+          },
+          create () {
+
+          },
+          update () {
+
+          }
+        }
+      }
+      // eslint-disable-next-line
+      new Phaser.Game(config)
+    }
+  }
+</script>
