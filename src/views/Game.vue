0bd66293ec31846dc075cb691991f1a2754633ef
mirokshi <rojasdiazmirokshi@gmail.com>
2019-05-02 21:27:41 +0200

    Sync

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,40 +1,53 @@
+
 <template>
   <div id="game"></div>
 </template>
+  <script>
+    /* eslint-disable */
+    import Phaser from 'phaser'
+    import _TILESET_SET from '../assets/tileset.png'
+    import _JSON_MAP from '../assets/map.json'
 
-<script>
-  import Phaser from 'phaser'
-  import background from '../assets/background.jpg'
-  export default {
-    name: 'Game',
-    mounted () {
-      // PHASER 2.6 -> phaser-ce
-      // PHASER 3.0 -> phaser
-      let config = {
-        type: Phaser.AUTO,
-        width: 500,
-        height: 200,
-        physics: {
-          default: 'arcade',
-          arcade: {
-            gravity: { y: 200 }
-          }
-        },
-        // NO HI HA STATES A 3.0 -> SCENES
-        scene: {
-          preload () {
-              this.load.image('background', background)
+    export default {
+      name: 'Game',
+      created() {
+        var controls = false
+        // PHASER 2.6 -> phaser-ce
+        // PHASER 3.0 -> phaser
+        let config = {
+          type: Phaser.AUTO,
+          width: window.innerWidth -25,
+          height: window.innerHeight -20,
+          physics: {
+            default: 'arcade',
+            arcade: {
+              gravity: {y: 200}
+            }
           },
-          create () {
+          // NO HI HA STATES A 3.0 -> SCENES
+          scene: {
+            preload() {
+              this.load.image('tileset', _TILESET_SET)
+              this.load.tilemapTiledJSON("map",_JSON_MAP)
+            },
+            create() {
+              // let map = this.make.tilemap({ key: "_JSON_MAP" })
+              // let tileset = map.addTilesetImage("game", "tileset")
+              // map.createStaticLayer("Background", tileset, 0, 0)
+              // map.createStaticLayer("walls", tileset, 0, 0)
 
-          },
-          update () {
 
+
+
+            },
+            update() {
+
+            }
           }
         }
+        // eslint-disable-next-line
+        new Phaser.Game(config)
       }
-      // eslint-disable-next-line
-      new Phaser.Game(config)
     }
-  }
-</script>
+  </script>
+
