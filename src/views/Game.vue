7c1af5176d93b1dbf4657eefcabe4e5e9e3edae5
mirokshi <rojasdiazmirokshi@gmail.com>
2019-05-06 19:41:05 +0200

    Sync

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -28,16 +28,22 @@
           // NO HI HA STATES A 3.0 -> SCENES
           scene: {
             preload() {
+              console.log("PRELOAD");
               this.load.image('tileset', _TILESET_SET)
+              this.load.spritesheet('player',_PLAYER_BOMB,{frameWidth:28,frameHeight:22})
               this.load.tilemapTiledJSON("map",_JSON_MAP)
             },
             create() {
+              console.log("CREATED");
+              this.cameras.main.backgroundColor.setTo(52,152,219)
+              this.level = this.physics.add.staticGroup()
               let map = this.make.tilemap({ key: "map" })
               let tileset = map.addTilesetImage("tilesets", "tileset")
               map.createStaticLayer("background", tileset, 0, 0)
               map.createStaticLayer("floor", tileset, 0, 0)
 
-              this.cameras.main.setZoom(0.5)
+              // this.cameras.main.setZoom(0.5)
+              this.cameras.main.startFollow()
             },
             update() {
 
