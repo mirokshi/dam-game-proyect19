26d95d7a9d2765a4aec65ce8cd49f4be4f647eaf
mirokshi <rojasdiazmirokshi@gmail.com>
2019-05-29 14:01:13 +0200

    Sync

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -6,28 +6,24 @@
     import Phaser from 'phaser'
     import _TILESET_SET from '../assets/tileSets/tileset.png'
     import _JSON_MAP from '../assets/tileSets/mapa.json'
+    import _JSON_MAP2 from '../assets/tileSets/mapa2.json'
     import _PLAYER from '../assets/characters/cyclops.png'
     import _ENEMIES from '../assets/characters/imp.png'
     import _OBJECTS from '../assets/objects/objects.png'
-    import _PARTICLES from '../assets/particles/rain.png'
+    import _DEMON from '../assets/characters/demon160x128.png'
+    import _PARTICLES from '../assets/particles/pp/circle_01.png'
     //sounds
     import _GAME_MUSIC from '../assets/sounds/wapons.mp3'
     import _SOUND_EAT from '../assets/sounds/eat.mp3'
 
 
-    let camera;
-    let player;
-    let CoinLayer;
-    let coins;
-    let EnemyLayer;
-    let enemies;
-    let coinScore = 0;
+    let camera, player, demon;
+    let coins,CoinLayer,coinScore = 0;
+    let EnemyLayer, enemies;
     let scoreText;
-    let emitter;
+    let particles, emitter;
     let soundEat
-    let lives = 3;
-    let livesText;
-
+    let lives = 3,livesText;
     function changeDirection (enemy) {
       enemy.speedX*=-1
     }
@@ -48,23 +44,27 @@
     }
     function createEnemies () {
       EnemyLayer.forEach(object => {
-        let obj = enemies.create(object.x, object.y, 'enemy')
+        let obj = enemies.create(object.x, object.y, 'enemy',1)
         obj.setScale(1.5)
         // obj.setScale(object.width / 64, object.height / 64)
         // obj.setOrigin(0)
         obj.body.width = object.width
         obj.body.height = object.height
-        obj.speedX = 50
+        obj.speedX = 70
 
       })
     }
-    function lesslive (player, enemy) {
 
+    function lesslive (player, enemy) {
       lives = lives - 1
+      player.scene.cameras.main.shake(250)
+      player.x =150
+      player.y=150
       // player.disableBody(true, true)
       livesText.setText('Lives : '+lives)
-      player.scene.cameras.main.shake(500)
-
+    }
+    function winGame1(){
+      this.scene.start('inGame2')
     }
 
     export default {
@@ -85,7 +85,7 @@
             }
           },
           // NO HI HA STATES A 3.0 -> SCENES
-          scene: [loader, inGame]
+          scene: [loader, inGame, inGame2]
         }
         // eslint-disable-next-line
         new Phaser.Game(config)
@@ -99,9 +99,9 @@
         var progressBar = this.add.graphics()
         var progressBox = this.add.graphics()
         progressBox.fillStyle(0x222222, 0.8)
-        progressBox.fillRect(545, 320, 320, 50)
         var width = this.cameras.main.width
         var height = this.cameras.main.height
+        progressBox.fillRect(window.x = width / 2 - 165, window.y = height / 2 - 75, 320, 50)
         var loadingText = this.make.text({
           x: width / 2,
           y: height / 2 - 50,
@@ -136,7 +136,7 @@
           percentText.setText(parseInt(value * 100) + '%')
           progressBar.clear()
           progressBar.fillStyle(0xffffff, 1)
-          progressBar.fillRect(550, 330, 300 * value, 30)
+          progressBar.fillRect(window.x = width / 2 - 165, window.y = height / 2 - 65, 300 * value, 30)
         })
         this.load.on('fileprogress', function (file) {
           assetText.setText('Loading asset: ' + file.key)
@@ -150,10 +150,14 @@
         })
         this.load.image('tileset', _TILESET_SET)
         this.load.tilemapTiledJSON('map', _JSON_MAP)
+        this.load.tilemapTiledJSON('map2', _JSON_MAP2)
         this.load.spritesheet('player',_PLAYER ,{ frameWidth: 64, frameHeight: 64 })
         this.load.spritesheet('objects',_OBJECTS ,{ frameWidth: 32, frameHeight: 32 })
         this.load.spritesheet('enemy',_ENEMIES ,{ frameWidth: 32, frameHeight: 32 })
+        this.load.spritesheet('demon',_DEMON ,{ frameWidth: 160, frameHeight: 128 })
         // this.load.spritesheet('rain',_PARTICLES,{frameWidth:17,frameHeight:17})
+
+        this.load.image('particles',_PARTICLES)
 
         this.load.audio('sound',_GAME_MUSIC)
         this.load.audio('eat',_SOUND_EAT)
@@ -185,6 +189,7 @@
         let tileset = map.addTilesetImage("tileset", "tileset")
         // let tilest_object = map.addTilesetImage("objects", "objects")
         let backgroundP =map.createStaticLayer("backgroundP", tileset, 0, 0)
+        let end = map.createStaticLayer("end",tileset,0,0)
         let collision = map.createStaticLayer("collisions",tileset,0,0)
         let backgroundS =map.createStaticLayer("backgroundS", tileset, 0, 0)
         let details = map.createStaticLayer("details", tileset, 0, 0)
@@ -194,29 +199,17 @@
 
         coins = this.physics.add.staticGroup()
         enemies = this.physics.add.group()
+        // demon = this.physics.add.group()
         // enemies.enableBody=true;
 
         backgroundP.setCollisionByExclusion([-1]);
+        end.setCollisionByExclusion([-1]);
         collision.setCollisionByExclusion([-1]);
         backgroundS.setCollisionByExclusion([-1]);
         details.setCollisionByExclusion([-1]);
         walls.setCollisionByExclusion([-1]);
 
-        // //Particles ->rain
-        // emitter= this.add.emitter(this.world.centerX,0,400);
-        // emitter.width = this.world.width;
-        // emitter.makeParticles('rain');
-        //
-        // emitter.minParticleScale = 0.1;
-        // emitter.maxParticleScale = 0.5;
-        //
-        // emitter.setYSpeed(300, 500);
-        // emitter.setXSpeed(-5, 5);
-        //
-        // emitter.minRotation = 0;
-        // emitter.maxRotation = 0;
-        //
-        // emitter.start(false, 1600, 5, 0);
+
 
         // La càmara no sortirà del món
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
@@ -226,8 +219,24 @@
         player = this.physics.add.sprite(spawnpoint.x, spawnpoint.y, 'player');
         player.body.setSize(24, 64, 40, 0)
         player.lives = 3;
-        // Habilitem un cursor per a les fletxes del teclat
+
+
+        // Habilitar flechas
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        particles = this.add.particles('particles');
+
+        emitter = particles.createEmitter({
+          x:player.body.x + player.body.width /2 ,
+          y: player.body.y + player.body.height,
+          angle: { min: 140, max: 180 },
+          speed: 200,
+          gravityY: 300,
+          scale: { start: 0.1, end: 0 },
+          lifespan: { min: 500, max: 1000 },
+          blendMode: 'ADD'
+        })
+
         //Animacion parado
         this.anims.create({
           key:'idle',
@@ -261,14 +270,27 @@
           frameRate: 51,
           repeat: -1
         });
+        // this.anims.create({
+        //   key:'idleDemon',
+        //   frames:this.anims.generateFrameNames('demon',{
+        //     frames:[0,1,2,3,4,5]
+        //   }),
+        //   frameRate:1,
+        //   repeat: -1
+        // })
         createCoins()
         createEnemies()
+        // createDemon()
 
         this.physics.add.collider(player,walls)
+        this.physics.add.collider(player,enemies,lesslive)
+        this.physics.add.collider(player,end,winGame1,null,this)
+        this.physics.add.overlap(player,coins,takeCoin,null,this)
         this.physics.add.collider(enemies,walls)
         this.physics.add.collider(enemies,collision,changeDirection)
-        this.physics.add.collider(player,enemies,lesslive)
-        this.physics.add.overlap(player,coins,takeCoin,null,this)
+        // this.physics.add.collider(demon,walls)
+        // this.physics.add.collider(demon,player)
+        console.log(demon);
 
         camera.setZoom(2)
         camera.startFollow(player)
@@ -301,6 +323,190 @@
           player.anims.play('walk', true)
           player.setVelocityX(-200)
           player.flipX = true
+          emitter.setPosition(player.body.x + player.body.width, player.body.y + player.body.height)
+        }else if (this.cursors.right.isDown) {
+          player.anims.play('walk', true)
+          player.setVelocityX(200)
+          player.flipX = false
+          emitter.setPosition(player.body.x, player.body.y + player.body.height)
+        } else {
+          player.anims.play('idle',true)
+          player.setVelocityX(0)
+          emitter.setPosition(player.body.x + player.body.width / 2, player.body.y + player.body.height)
+        }
+
+        if (this.cursors.up.isDown && player.body.onFloor()) {
+          player.setFrame(14)
+          player.setVelocityY(-300)
+        }
+        if(player.body.velocity.y < 0){
+          player.setFrame(14)
+        }
+        if(player.body.velocity.y > 0){
+          player.setFrame(17)
+          player.body.setMaxVelocity(400, 550);
+        }
+      }
+    }
+    class inGame2 extends Phaser.Scene{
+      constructor (){
+        super({key:'inGame2'})
+      }
+      preload(){
+        console.log('PRELOAD');
+      }
+      create(){
+        camera = this.cameras.main;
+        console.log("CREATED");
+
+
+        //musica
+        let music = this.sound.add('sound', { loop: true })
+        //Souns
+        soundEat = this.sound.add('eat', {loop: false})
+        // sound.play()
+
+        let map = this.make.tilemap({ key: "map2" })
+        let tileset = map.addTilesetImage("tileset", "tileset")
+        let backgroundP =map.createStaticLayer("backgroundP", tileset, 0, 0)
+        let end = map.createStaticLayer("end",tileset,0,0)
+        let collision = map.createStaticLayer("collisions",tileset,0,0)
+        let backgroundS =map.createStaticLayer("backgroundS", tileset, 0, 0)
+        let details = map.createStaticLayer("details", tileset, 0, 0)
+        let walls = map.createStaticLayer("walls",tileset,0,0)
+        CoinLayer = map.getObjectLayer('coins')['objects']
+        EnemyLayer = map.getObjectLayer('enemies')['objects']
+
+        coins = this.physics.add.staticGroup()
+        enemies = this.physics.add.group()
+        // demon = this.physics.add.group()
+        // enemies.enableBody=true;
+
+        backgroundP.setCollisionByExclusion([-1]);
+        end.setCollisionByExclusion([-1]);
+        collision.setCollisionByExclusion([-1]);
+        backgroundS.setCollisionByExclusion([-1]);
+        details.setCollisionByExclusion([-1]);
+        walls.setCollisionByExclusion([-1]);
+
+        // //Particles ->rain
+        // emitter= this.add.emitter(this.world.centerX,0,400);
+        // emitter.width = this.world.width;
+        // emitter.makeParticles('rain');
+        //
+        // emitter.minParticleScale = 0.1;
+        // emitter.maxParticleScale = 0.5;
+        //
+        // emitter.setYSpeed(300, 500);
+        // emitter.setXSpeed(-5, 5);
+        //
+        // emitter.minRotation = 0;
+        // emitter.maxRotation = 0;
+        //
+        // emitter.start(false, 1600, 5, 0);
+
+        // La càmara no sortirà del món
+        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        //Aparicion del personaje
+        let spawnpoint = map.findObject('player', obj => obj.name === 'spawnpoint');
+        player = this.physics.add.sprite(spawnpoint.x, spawnpoint.y, 'player');
+        player.body.setSize(24, 64, 40, 0)
+        player.lives = 3;
+
+        //Demon
+        let spawnDemon = map.findObject('demon',obj => obj.name === 'spawndemon');
+        demon = this.physics.add.sprite(spawnDemon.x,spawnDemon.y, 'demon');
+
+        // Habilitar flechas
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        //Animacion parado
+        this.anims.create({
+          key:'idle',
+          frames:this.anims.generateFrameNames('player',{
+            frames: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+          }),
+          frameRate:7,
+          repeat:-1
+        });
+        // Animación de correr del player
+        this.anims.create({
+          key: 'walk',
+          frames: this.anims.generateFrameNames('player', {
+            frames: [15,16,17,18,19,20,21,22,23,24,25, 26]
+          }),
+          frameRate: 20,
+          repeat: -1
+        });
+        //Animacion de correr enemy
+        this.anims.create({
+          key:'walk_enemy',
+          frames:this.anims.generateFrameNumbers('enemy',{start:8,end:15}),
+          frameRate: 12,
+          repeat: -1
+        });
+        this.anims.create({
+          key: 'shotting',
+          frames: this.anims.generateFrameNames('player', {
+            frames: [45,46,47,48,49,50,51,52,53,54,55,56,57]
+          }),
+          frameRate: 51,
+          repeat: -1
+        });
+        // this.anims.create({
+        //   key:'idleDemon',
+        //   frames:this.anims.generateFrameNames('demon',{
+        //     frames:[0,1,2,3,4,5]
+        //   }),
+        //   frameRate:1,
+        //   repeat: -1
+        // })
+        createCoins()
+        createEnemies()
+        // createDemon()
+
+        this.physics.add.collider(player,walls)
+        this.physics.add.collider(player,enemies,lesslive)
+        this.physics.add.collider(player,end)
+        this.physics.add.overlap(player,coins,takeCoin,null,this)
+        this.physics.add.collider(enemies,walls)
+        this.physics.add.collider(enemies,collision,changeDirection)
+        this.physics.add.collider(demon,walls)
+        this.physics.add.collider(demon,player)
+        console.log(demon);
+
+        camera.setZoom(2)
+        camera.startFollow(player)
+
+        scoreText=this.add.text(camera.centerX-350,camera.centerY-170,'Score: 0',{fontSize:'18px',fill:'#000'})
+        scoreText.setColor('#ffffff')
+        scoreText.setScrollFactor(0)
+
+        livesText=this.add.text(camera.centerX-350,camera.centerY-150,'Lives : 3',{fontSize:'18px',fill:'#000'})
+        livesText.setColor('#ffffff')
+        livesText.setScrollFactor(0)
+        // music.play()
+
+      }
+      update(){
+        if (enemies){
+          enemies.children.entries.forEach(function (enemy) {
+            enemy.setVelocityX(enemy.speedX)
+            if (enemy.speedX>0){
+              enemy.anims.play('walk_enemy', true)
+              enemy.flipX = false
+            }else{
+              enemy.flipX = true
+            }
+            // enemy.setVelocityX(-200)
+
+          })
+        }
+        if (this.cursors.left.isDown) {
+          player.anims.play('walk', true)
+          player.setVelocityX(-200)
+          player.flipX = true
         }else if (this.cursors.right.isDown) {
           player.anims.play('walk', true)
           player.setVelocityX(200)
@@ -311,7 +517,7 @@
         }
 
         if (this.cursors.up.isDown && player.body.onFloor()) {
-          player.setFrame()
+          player.setFrame(14)
           player.setVelocityY(-300)
         }
         if(player.body.velocity.y < 0){
@@ -321,9 +527,7 @@
           player.setFrame(17)
           player.body.setMaxVelocity(400, 550);
         }
-
       }
     }
-
   </script>
 
